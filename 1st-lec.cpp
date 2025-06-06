fd51674a1853cadcf756c145e9386f58fe17e86f
Evans <evansmenyah7@gmail.com>
2025-06-06 15:43:10 -0800

    trials

---
+++ 1st-lec.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   // int i = 0;
+ //  i++;
+  // cout <<"The current value of 'i' is " << i <<endl;
+  double k = 23.5;
+  k--;
+  cout <<"the current value of k is" <<k<<endl;
+    return 0;
+}
---
+++ 1st-lecqu.cpp
@@ -0,0 +1,11 @@
+//Output the sum of the first 10 natural numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 1;
+    while (num < 11);
+    num++;
+    cout << num++ << endl;
+    return 0;
+}
